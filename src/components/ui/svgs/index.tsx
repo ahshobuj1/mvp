421cbc4f2f13b0befcff0e1c82861b5b2249c051
A H Shobuj <ahshobuj09@gmail.com>
2026-03-31 20:54:09 +0600

    added hero

---
+++ src/components/ui/svgs/index.tsx
@@ -0,0 +1,67 @@
+import React from "react"
+
+export const Spotify = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.508 17.302c-.223.366-.7.483-1.066.26-2.917-1.783-6.589-2.186-10.915-1.197-.419.096-.843-.173-.939-.592-.096-.419.173-.843.592-.939 4.739-1.083 8.805-.623 12.068 1.373.366.223.483.7.26 1.066zm1.474-3.264c-.281.456-.881.604-1.337.323-3.338-2.052-8.428-2.645-12.37-1.447-.514.156-1.053-.134-1.21-.648-.156-.514.134-1.053.648-1.21 4.512-1.37 10.123-.711 13.949 1.639.456.282.604.881.321 1.337s-.001.006-.001.006zm.126-3.41c-4.004-2.378-10.609-2.599-14.444-1.434-.614.186-1.258-.161-1.444-.775-.186-.614.161-1.258.775-1.444 4.407-1.337 11.696-1.08 16.3 1.652.553.328.739 1.041.411 1.594-.328.552-1.042.738-1.594.411l-.004-.004z" />
+  </svg>
+)
+
+export const VercelFull = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 285 64" fill="currentColor" {...props}>
+    <path d="M36.12 0l36.12 62.56H0L36.12 0zM104.75 42.14V17.07h12.78c8.35 0 13.4 3.99 13.4 12.3 0 7.96-5.05 12.77-13.4 12.77h-12.78zm4.4-20.73v16.39h8.38c5.44 0 8.92-2.53 8.92-8.3 0-5.49-3.48-8.09-8.92-8.09h-8.38zM141.6 42.14V17.07h17.95v4.34h-13.55v6.04h13.25v4.3h-13.25v6.08h13.55v4.31h-17.95zm30.34-12.53c0-7.88 5.76-13.12 13.59-13.12s13.59 5.24 13.59 13.12-5.76 13.12-13.59 13.12-13.59-5.24-13.59-13.12zm4.52 0c0 5.4 3.65 8.78 9.07 8.78s9.07-3.38 9.07-8.78-3.65-8.78-9.07-8.78-9.07 3.38-9.07 8.78zM207.12 42.14l-6.84-25.07h4.7l4.57 19.38 4.67-19.38h4.63l4.63 19.38 4.6-19.38h4.67l-6.84 25.07h-4.7l-4.7-19.12-4.7 19.12h-4.7zM245.89 31.81l7.85-14.74h5.05l-10.37 18.06v8.94h-4.47V35.13l-10.34-18.06h5.05l7.73 14.74zM269.41 42.14V17.07h15.42c8.28 0 13.22 3.86 13.22 12.3s-4.94 12.77-13.22 12.77h-15.42zm4.4-20.73v16.39h11.02c5.44 0 8.82-2.53 8.82-8.3s-3.38-8.09-8.82-8.09h-11.02z" />
+  </svg>
+)
+
+export const SupabaseFull = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 120 24" fill="currentColor" {...props}>
+    <path d="M11.6 0l-1.5 8.1c-.2.9.5 1.7 1.4 1.7h7.9c1 0 1.5 1.1.9 1.8L9 24l1.5-8.1c.2-.9-.5-1.7-1.4-1.7H1.2c-1 0-1.5-1.1-.9-1.8L11.6 0z" />
+    <text x="30" y="18" fontWeight="bold" fontSize="16">Supabase</text>
+  </svg>
+)
+
+export const Hulu = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 56 18" fill="currentColor" {...props}>
+    <path d="M12.9 6.8v8h-3V11c0-1.8-1-2.9-2.5-2.9s-2.6 1.1-2.6 3v3.7h-3v-8h3v1.1c.7-.7 1.7-1.3 3-1.3 2.7.2 5.1 1.7 5.1 4.7zM26.2 14.8h-3v-1.1c-.7.7-1.7 1.3-3 1.3-2.7 0-5.1-1.5-5.1-4.5V2.5h3V11c0 1.8 1 2.9 2.5 2.9s2.6-1.1 2.6-3V2.5h3v12.3zM39.6 2.5v12.3h-3v-1.1c-.7.7-1.7 1.3-3 1.3-2.7 0-5.1-1.5-5.1-4.5V2.5h3V11c0 1.8 1 2.9 2.5 2.9s2.6-1.1 2.6-3V2.5h3zm13.4 12.3h-3v-1.1c-.7.7-1.7 1.3-3 1.3-2.7 0-5.1-1.5-5.1-4.5V2.5h3V11c0 1.8 1 2.9 2.5 2.9s2.6-1.1 2.6-3V2.5h3v12.3z" />
+  </svg>
+)
+
+export const Bolt = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M13 10V3L4 14H11V21L20 10H13Z" />
+  </svg>
+)
+
+export const FirebaseFull = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 120 24" fill="currentColor" {...props}>
+    <path fill="#FFCA28" d="M3.89 15.67L5.26 7.12C5.35 6.54 5.92 6.25 6.43 6.54L15.35 11.66L3.89 15.67Z" />
+    <path fill="#FF8F00" d="M16.14 11.23L13.89 2.21C13.78 1.78 13.25 1.63 12.92 1.94L3.89 15.67L16.14 11.23Z" />
+    <path fill="#DE3905" d="M16.63 16.59L15.54 9.17C15.48 8.78 14.96 8.71 14.81 9.07L3.89 15.67L10.37 21.65C10.74 22 11.3 22 11.66 21.65L16.63 16.59Z" />
+    <text x="30" y="18" fontWeight="bold" fontSize="16">Firebase</text>
+  </svg>
+)
+
+export const Beacon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <circle cx="12" cy="12" r="10" />
+    <text x="5" y="16" fill="white" fontWeight="bold" fontSize="10">B</text>
+  </svg>
+)
+
+export const Claude = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 90 26" fill="currentColor" {...props}>
+    <text x="0" y="20" fontWeight="bold" fontSize="18">Claude</text>
+  </svg>
+)
+
+export const Figma = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <path d="M12 0C8.686 0 6 2.686 6 6v12c0 3.314 2.686 6 6 6s6-2.686 6-6V6c0-3.314-2.686-6-6-6zm0 2c1.105 0 2 .895 2 2v2H10V4c0-1.105.895-2 2-2zM8 6h3v4H8V6zm5 0h3v4h-3V6zM8 12h3v4H8v-4zm5 0h3v4h-3v-4zm-1 8c-1.105 0-2-.895-2-2h4c0 1.105-.895 2-2 2z" />
+  </svg>
+)
+
+export const Cisco = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 60 30" fill="currentColor" {...props}>
+    <path d="M10 20v5m5-8v8m5-10v10m5-12v12m5-12v12m5-10v10m5-8v8m5-5v5" stroke="currentColor" strokeWidth="2" />
+    <text x="15" y="28" fontSize="8">CISCO</text>
+  </svg>
+)
